f373aa1cdeecc1e04cbc0af390f10367c927e723
Daniel Phan <107604542+danielphandptp@users.noreply.github.com>
2022-07-01 14:02:07 -0400

    added basic functions to work with simple jsx files, including imports

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JavaScript Jupyter Kernel
 
+## Overview
+
 ## References
+https://unpkg.com/
+https://esbuild.github.io/
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,74 @@
-import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-	return <h1>hello there</h1>
+	const ref = useRef<any>();
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	const onChange = (event: React.ChangeEvent<HTMLTextAreaElement> | undefined) => {
+		if (event) {
+			console.log(event.target.value);
+			setInput(event.target.value);
+		}
+	}
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async (event: React.MouseEvent<HTMLButtonElement> | undefined) => {
+		if (!event || !ref.current) {
+			return;
+		}
+		const result = await ref.current.build({
+			entryPoints: ['index.js'],
+			bundle: true,
+			write: false,
+			plugins: [unpkgPathPlugin()]
+		});
+		console.log(result);
+		setCode(result.outputFiles[0].text);
+	};
+
+	return (
+		<div>
+			{/* ------------------ */}
+			{/* Area to enter code */}
+			{/* ------------------ */}
+			<textarea
+				value={input}
+				onChange={onChange}>
+			</textarea>
+			
+			{/* -------------	*/}
+			{/* Submit button */}
+			{/* ------------- */}
+			<div>
+				<button
+					onClick={onClick}>
+					Submit
+				</button>
+			</div>
+
+			{/* ----------------------------------- */}
+			{/* Display area of the transpiled code */}
+			{/* ----------------------------------- */}
+			<pre>
+				{code}
+			</pre>
+		</div>
+	)
 };
 
-ReactDOM.render(
-	<App />,
-	document.querySelector('#root')
-);
+const root = ReactDOM.createRoot(document.querySelector('#root')!);
+root.render(<App />)
