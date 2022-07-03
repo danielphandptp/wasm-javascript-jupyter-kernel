498e1dafe38603ee28addd513ab6e6995b5291e2
danielphandptp <>
2022-07-03 14:06:36 -0400

    bug fixes: de-bouncing (the application became laggy when the window is being resized quickly)

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -11,20 +12,41 @@ const Resizable: React.FunctionComponent<ResizableProps> = ({
   children,
 }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+      }, 100);
+    };
+    window.addEventListener('resize', listener);
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
   if (direction === 'horizontal') {
     resizableProps = {
-      width: window.innerWidth * 0.8,
+      className: 'resize-horizontal',
+      width: window.innerWidth * 0.75,
       height: Infinity,
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.8, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       resizeHandles: ['e'],
     };
   } else if (direction === 'vertical') {
     resizableProps = {
       width: Infinity,
       height: 300,
-      minConstraints: [Infinity, window.innerHeight * 0.2],
-      maxConstraints: [Infinity, window.innerHeight * 0.8],
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       resizeHandles: ['s'],
     };
   } else {
